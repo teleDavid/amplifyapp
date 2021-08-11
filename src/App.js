a23000c6835fd4e3a7368c0e92ef3776c4e23cba
teleDavid <david@telecomstack.com>
2021-08-11 13:55:32 +0100

    Email Test7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-var AWS = require('aws-sdk');
+//var AWS = require('aws-sdk');
 
 /*var params = {
   MessageBody: 'Teststststtststststststststtststststststststststt',
