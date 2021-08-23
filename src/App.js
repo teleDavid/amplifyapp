356b059b5a0cd4713af8635c41dd7a1386c97740
teleDavid <david@telecomstack.com>
2021-08-23 15:30:23 +0100

    EmailValidationTest30

--- src/App.js
+++ src/App.js
@@ -16,6 +16,45 @@ import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 
 function App() {
+
+  const [bucketName, setBucketName] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  // Replace REGION with the appropriate AWS Region, such as 'us-east-1'.
+  const region = "eu-west-1";
+  const client = new S3Client({
+    region,
+    credentials: fromCognitoIdentityPool({
+      client: new CognitoIdentityClient({ region }),
+      // Replace IDENTITY_POOL_ID with an appropriate Amazon Cognito Identity Pool ID for, such as 'us-east-1:xxxxxx-xxx-4103-9936-b52exxxxfd6'.
+      identityPoolId: "eu-west-1:92b669d5-3f6f-42e4-ac66-937802f7c608",
+    }),
+  });
+
+  const createBucket = async () => {
+    setSuccessMsg("");
+    setErrorMsg("");
+
+    try {
+      await client.send(new CreateBucketCommand({ Bucket: bucketName }));
+      setSuccessMsg(`Bucket "${bucketName}" created.`);
+    } catch (e) {
+      setErrorMsg(e);
+    }
+  };
+
+  const deleteBucket = async () => {
+    setSuccessMsg("");
+    setErrorMsg("");
+
+    try {
+      await client.send(new DeleteBucketCommand({ Bucket: bucketName }));
+      setSuccessMsg(`Bucket "${bucketName}" deleted.`);
+    } catch (e) {
+      setErrorMsg(e);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
